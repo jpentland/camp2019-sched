67eeb491d1a27732d8c35782f742766df146ae6e
Joseph Pentland <joe.pentland@gmail.com>
2019-08-21 19:41:54 +0200

    fix typo

--- termux_setup.sh
+++ termux_setup.sh
@@ -2,5 +2,5 @@
 set -e
 read -p "This should only be run on termux, press ctrl+c to exit or return to continue"
 apt update
-apt install -y clang python libiconv libxml2 libxlt zlib
+apt install -y clang python libiconv libxml2 libxslt zlib
 pip install lxml pytz python-dateutil requests
