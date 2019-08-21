2b3355d89faec6407937010b12442fc672f245c7
Joseph Pentland <joe.pentland@gmail.com>
2019-08-21 19:37:14 +0200

    add termux setup script to install dependencies

---
+++ termux_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+read -p "This should only be run on termux, press ctrl+c to exit or return to continue"
+apt update
+apt install -y clang python libiconv libxml2 libxlt zlib
+pip install lxml pytz python-dateutil requests
